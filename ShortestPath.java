f37a576ea7150057ba6d5b03c844eca2038c9d53
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2019-12-27 10:47:44 +0530

    Update ShortestPath.java

--- ShortestPath.java
+++ ShortestPath.java
@@ -1,11 +1,26 @@
-/* Solution for shortest path problem */
+/*
+
+A men just shifted to a new house. He has to find the shortest path to reach his office.
+The key points for the problem are:- He can move as much as possible in any direction,
+He is to reach his office by shortest path.
+The input will be provided as a string.
+The output should be a string.
+All the path will be E,W,N and S as East, West, North and South respectively.
+
+*/
 
 import java.util.Scanner;
 
 public class ShortestPath {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        StringBuffer str = new StringBuffer(sc.next());
+        String input;
+        input = sc.next();
+        StringBuffer str = new StringBuffer(input.toUpperCase());
+
+
+        System.out.println("Your input is: "+str);
+
         for (int i=0; i<str.length(); i++){
             if (str.charAt(i)=='N'){
                 for (int j=0; j<str.length();j++){
@@ -43,7 +58,10 @@ public class ShortestPath {
                 }
             }
         }
+
+
         String str2 = str.toString();
+
         if (str2.length()==0){
             System.out.println("Already in office!");
         }
